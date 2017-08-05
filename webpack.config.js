22654b9b3b501e7e90f98c6b7cb8487b52d19030
rogeralbinoi <roger.albino.i@gmail.com>
2017-08-05 15:24:23 -0300

    add: first commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './ex/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 4000,
+        contentBase: './public'
+    }
+}
